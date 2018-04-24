977c8c3d8fa090122a9022aa3fafc9d3c4824152
Matthew Eldredge <mattheweldredge@gmail.com>
2018-04-23 17:44:12 -0700

    return a list with items added from form and display

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-  const addItems = document.querySelector('.add-items');
-  const itemsList = document.querySelector('.plates');
-  const items = [];
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = [];
 
 
-  function addItem(e) {
-    e.preventDefault();
-    const text = (this.querySelector('[name=item]')).value();
-    const item = {
-        text : text,
-        done : flase
-    };
-    items.push(item);
-    this.reset();
-  }
-  // listen for submit and grab form items 
-  addItems.addEventListener('submit', addItem);
+function addItem(e) {
+e.preventDefault();
+const text = (this.querySelector('[name=item]')).value;
+const item = {
+    text : text,
+    done : false
+};
+items.push(item);
+populateList(items, itemsList);
+this.reset();
+}
+
+function populateList(items = [], itemList) {
+    //create an HTML list to display data
+    itemList.innerHTML = items.map((item, index) => {
+        return `
+            <li>
+                <label for="">${item.text}</label>
+            </li>
+        `
+    }).join(''); //join array into one string
+}
+
+// listen for submit and grab form items 
+addItems.addEventListener('submit', addItem);
