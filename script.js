b6a5f6c75ba424518bc26671e697d6e8e7bb7e35
Matthew Eldredge <mattheweldredge@gmail.com>
2018-04-23 17:30:11 -0700

    add event listener to handle add item

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
   const addItems = document.querySelector('.add-items');
   const itemsList = document.querySelector('.plates');
   const items = [];
-  
+
+
+  function addItem(e) {
+    console.log('hello');
+  }
+  // listen for submit and grab form items 
+  additems.addEventListener('submit', addItem);
